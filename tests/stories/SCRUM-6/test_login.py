095d02761287d7536458c36f6b99b8a6872dd39b
gauthamgits <65800623+gauthamgits@users.noreply.github.com>
2026-06-10 22:55:18 +1000

    Update generated tests for SCRUM-6

--- tests/stories/SCRUM-6/test_login.py
+++ tests/stories/SCRUM-6/test_login.py
@@ -1,33 +1,47 @@
 import pytest
-from tests.pages.login_page import LoginPage
+from tests.pages.login_practice_page import LoginPracticePage
+
 
 class TestLogin:
+    
     @pytest.fixture(autouse=True)
     def setup(self, page, base_url):
+        self.login_page = LoginPracticePage(page, base_url)
         self.page = page
-        self.login_page = LoginPage(page, base_url)
-        self.test_url = "https://rahulshettyacademy.com/loginpagePractise"
-
+        
     def test_login_with_valid_credentials_navigates_to_homepage(self):
-        self.login_page.navigate_to_login(self.test_url)
-        self.login_page.enter_username("rahulshettyacademy")
-        self.login_page.enter_password("Learning@830$3mK2)")
-        self.login_page.select_terms_checkbox()
-        self.login_page.click_signin()
-
-        #self.page.wait_for_timeout(3000)
-        current_url = self.login_page.get_current_url()
-        assert current_url != self.test_url, f"Expected redirect but stayed on {current_url}"
-
+        """
+        AC1: Given the test endpoint, valid username and password,
+        when I try to login with the provided credentials and select terms checkbox,
+        then I should be able to login successfully and be navigated to the homepage.
+        """
+        self.login_page.navigate_to_login()
+        self.login_page.login(
+            username="rahulshettyacademy",
+            password="Learning@830$3mK2)",
+            accept_terms=True
+        )
+        
+        self.page.wait_for_url("**/angularpractice/shop", timeout=10000)
+        
+        assert "shop" in self.page.url, "User should be navigated to homepage after successful login"
+        
     def test_login_with_incorrect_credentials_shows_error_message(self):
-        self.login_page.navigate_to_login(self.test_url)
-        self.login_page.enter_username("incorrectuser")
-        self.login_page.enter_password("wrongpassword")
-        self.login_page.select_terms_checkbox()
-        self.login_page.click_signin()
-
-        # Wait for alert to appear
-        self.page.wait_for_selector(".alert", timeout=5000)
-        assert self.login_page.is_error_message_visible()
+        """
+        AC2: Given the test endpoint,
+        when I try to login with incorrect username and password and select terms checkbox,
+        then I should see an error message: "Incorrect username/password."
+        """
+        self.login_page.navigate_to_login()
+        self.login_page.login(
+            username="invaliduser",
+            password="invalidpassword",
+            accept_terms=True
+        )
+        
+        self.page.wait_for_selector(self.login_page.error_message, timeout=5000)
+        
+        assert self.login_page.is_error_message_visible(), "Error message should be visible"
+        
         error_text = self.login_page.get_error_message()
-        assert "Incorrect username/password." in error_text
+        assert "Incorrect username/password." in error_text, f"Expected error message not found. Got: {error_text}"
